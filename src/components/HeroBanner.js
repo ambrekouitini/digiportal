00b2509d6409b68e84a0ff7414a125b72be334c7
Brevin <renaudbrevin@gmail.com>
2024-09-05 15:17:45 +0200

    :tada: Create presets, mixins, architecture

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,19 +1,15 @@
-import React, { useEffect, useRef } from 'react';
-import './heroBanner.scss';
+import React, { useRef } from 'react';
+import '../styles/heroBanner.scss';
 import { gsap } from 'gsap';
 
 const SectionOne = () => {
   const sectionRef = useRef(null);
 
-  useEffect(() => {
-    // Animation d'entrée avec GSAP
-    gsap.fromTo(sectionRef.current, { opacity: 0 }, { opacity: 1, duration: 1 });
-  }, []);
-
   return (
-    <section ref={sectionRef} className="section-one">
-      <h1>Bienvenue dans la première section</h1>
-      <p>Voici ma première section avec animation.</p>
+    <section ref={sectionRef} className="heroBanner">
+      <h2>Du style</h2>
+      <div className="textButton"></div>
+      <div class="button"></div>
     </section>
   );
 };
--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/styles.scss';
+import './styles/style.scss';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
