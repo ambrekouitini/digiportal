bd6eeb3ccd331f58164a05b248f8e6c8d1cfc2d4
Brevin <renaudbrevin@gmail.com>
2024-09-16 15:32:12 +0200

    :sparkles: Create header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,41 +1,70 @@
 import '../styles/header.scss';
-import { Link } from 'react-router-dom';
-
+import { Link, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import logo from '../assets/images/logo.svg';
 
+gsap.registerPlugin(ScrollTrigger);
+
 const Header = () => {
+    const location = useLocation();
 
+    const openNav = () => {
+        document.querySelector('nav').classList.toggle('nav-open');
+        document.querySelector('.burger').classList.toggle('close');
+        document.querySelector('.header').classList.toggle('header-open');
+    };
 
-return (
-    <div className="header">
-        <div className="navigation">
-            <Link to="/">
-                <img src={logo} alt="logo" />
-            </Link>
-            <div className="burger">
-                <span></span>
-                <span></span>
-                <span></span>
+    useEffect(() => {
+        document.querySelector('nav').classList.remove('nav-open');
+        document.querySelector('.burger').classList.remove('close');
+        document.querySelector('.header').classList.remove('header-open');
+    }, [location]); // Exécute cet effet à chaque changement d'URL
+
+    useEffect(() => {
+        // Animation de changement de couleur du header au scroll
+        gsap.to(".header", {
+            backgroundColor: "#333333",
+            scrollTrigger: {
+                trigger: ".header",
+                start: "top top", // Déclenche au début de la page
+                end: "1024px top", // Se termine après avoir scrollé 100vh
+                scrub: true, // Animation progressive liée au scroll
+            },
+        });
+    }, []);
+
+    return (
+        <div className="header">
+            <div className="navigation">
+                <Link to="/">
+                    <img src={logo} alt="logo" />
+                </Link>
+                <div className="burger" onClick={openNav}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
             </div>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Accueil</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">A propos</Link>
+                    </li>
+                    <li>
+                        <Link to="/services">Services</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">Contact</Link>
+                    </li>
+                </ul>
+            </nav>
         </div>
-        {/* <nav>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/about">About</Link>
-                </li>
-                <li>
-                    <Link to="/services">Services</Link>
-                </li>
-                <li>
-                    <Link to="/contact">Contact</Link>
-                </li>
-            </ul>
-        </nav> */}
-    </div>
-  );
+    );
 };
 
 export default Header;
